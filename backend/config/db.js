18ed443b5a2865fc7fe6400bd4a63bb1660c1794
Hariharan-Arjunan <hariharanarjunan@gmail.com>
2025-01-18 07:58:29 +0530

    data

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://hariharan19986:LtDgK3wAJC1oxRpk@cluster0.wvjlz.mongodb.net/products_db?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${conn?.connection?.host}`);
   } catch (error) {
     console.log(`Error : ${error?.message}`);
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,17 @@ import User from "./models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import verifyJWT from "./middleware/verifyJWT.js";
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.post("/api/register", async (req, res) => {
   const user = req.body;
@@ -145,7 +150,7 @@ app.delete("/api/products", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:5000", PORT);
 });
